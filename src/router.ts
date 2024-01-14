87aa3177916b9d1178568136d9cbea1cd98beeb8
GabrielEscudillo <gabrielescudillo@gmail.com>
2024-01-14 19:05:37 +0100

    fixing issues

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import userRoutes from "./routes/users.routes"
 import appointmentRoutes from "./routes/appointments.routes"
-
+import authRoutes from "./routes/auth.routes"
 const router = express.Router();
 
 router.use("/api", userRoutes);
 router.use("/api/get/appointments/", appointmentRoutes);
+router.use("/api/auth", authRoutes);
 
 export default router;
